6b884e4d0600281638efc064de02f6ebe55f36b1
anthonycheng <anthonycheng09@gmai.com>
2020-07-24 13:32:47 -0700

    recover back all test version

--- src/test/system/PlayerReaderTest.java
+++ src/test/system/PlayerReaderTest.java
@@ -12,33 +12,33 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 public class PlayerReaderTest {
 
-//    @Test
-//    void testParseAccountsFile1() {
-//        try {
-//            List<Player> players = PlayerReader.readAccounts(new File("./data/testPlayers2"));
-//            Player player1 = players.get(0);
-//            assertEquals("test1", player1.getUsername());
-//            assertEquals("pass1", player1.getPassword());
-//            assertEquals(0.0, player1.getRecord());
-//
-//            Player player2 = players.get(1);
-//            assertEquals("test2", player2.getUsername());
-//            assertEquals("pass2", player2.getPassword());
-//            assertEquals(1.0, player2.getRecord());
-//
-//        } catch (IOException e) {
-//            fail("IOException should not have been thrown");
-//        }
-//    }
-//
-//
-//
-//    @Test
-//    void testIOException() {
-//        try {
-//            PlayerReader.readAccounts(new File("./path/does/not/exist/testAccount.txt"));
-//        } catch (IOException e) {
-//            // expected
-//        }
-//    }
+    @Test
+    void testParseAccountsFile1() {
+        try {
+            List<Player> players = PlayerReader.readAccounts(new File("./data/testPlayers2"));
+            Player player1 = players.get(0);
+            assertEquals("test1", player1.getUsername());
+            assertEquals("pass1", player1.getPassword());
+            assertEquals(0.0, player1.getRecord());
+
+            Player player2 = players.get(1);
+            assertEquals("test2", player2.getUsername());
+            assertEquals("pass2", player2.getPassword());
+            assertEquals(1.0, player2.getRecord());
+
+        } catch (IOException e) {
+            fail("IOException should not have been thrown");
+        }
+    }
+
+
+
+    @Test
+    void testIOException() {
+        try {
+            PlayerReader.readAccounts(new File("./path/does/not/exist/testAccount.txt"));
+        } catch (IOException e) {
+            // expected
+        }
+    }
 }
