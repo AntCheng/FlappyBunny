769cd3eebe43ac8d6c5031172dfce88bc8ac5c31
anthonycheng <anthonycheng09@gmai.com>
2020-07-24 22:30:46 -0700

    normal test version, GameModelControllerTest seems to be the problem for autobot but is unclear how

--- src/test/model/CactusTest.java
+++ src/test/model/CactusTest.java
@@ -11,87 +11,87 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CactusTest {
 
-//    public GameModelController gmc;
-//    public Bunny bunny;
-//    public ArrayList<Floor> floorList;
-//    private Floor floor1;
-//    private Floor floor2;
-//    private Cactus cactus1;
-//    private Cactus cactus2;
-//    private ArrayList<Cactus> cactusList;
-//    private JFXPanel jfxPanel = new JFXPanel();
-//
-//    @BeforeEach
-//    void runBefore(){
-//        bunny = new Bunny(50,750);
-//        floor1 = new Floor(1000, 800,bunny,0);
-//        floor2 = new Floor(1000, 800,bunny,0);
-//        cactus1 = new Cactus(floor1);
-//        cactus2 = new Cactus(floor2);
-//        cactusList = new ArrayList<>();
-//        cactusList.add(cactus1);
-//        cactusList.add(cactus2);
-//    }
-//
-//    @Test
-//    void getPositionYTest(){
-//        assertEquals(floor1.getPositionY()-50,cactus1.getPositionY());
-//        assertEquals(floor2.getPositionY()-50,cactus2.getPositionY());
-//    }
-//
-//    @Test
-//    void getCactusImage(){
-//        assertEquals(cactus1.getImage().getWidth(),cactus2.getImage().getWidth());
-//        assertEquals(cactus2.getImage().getHeight(),cactus2.getImage().getHeight());
-//    }
-//
-//    @Test
-//    void checkIsTouchedTest() {
-//        cactus2.setPositionY(750);
-//        cactus2.setPositionX(50);
-//        cactus1.setPositionY(750);
-//        cactus1.setPositionX(75);
-//
-//        bunny.checkTouchCactus(cactusList);
-//        assertEquals(1,bunny.getHp());
-//        assertEquals(true,cactus1.getIsTounched());
-//        assertEquals(true,cactus1.getIsTounched());
-//    }
-//
-//    @Test
-//    void setIsTouchTest() {
-//        cactus1.setIsTouched(true);
-//        assertEquals(true,cactus1.getIsTounched());
-//    }
-//
-//
-//
-//    @Test
-//    void updateTest(){
-//        cactus1.setPositionX(500);
-//        cactus1.update(1);
-//        assertEquals(400,cactus1.getPositionX());
-//        assertEquals(101,cactus1.getVelocityX());
-//
-//        cactus1.update(4);
-//        assertEquals(-4,cactus1.getPositionX());
-//        assertEquals(102,cactus1.getVelocityX());
-//    }
-//
-//    @Test
-//    void getBoundaryTest() {
-//        assertEquals(new Rectangle2D(cactus1.getPositionX(),cactus1.getPositionY(),
-//                cactus1.getWidth() - 5,cactus1.getHeight() - 5), cactus1.getBoundary());
-//    }
-//
-//    @Test
-//    void intersectsTest() {
-//        cactus1.setPositionX(500);
-//        cactus1.setPositionY(500);
-//        bunny.setPositionX(500);
-//        bunny.setPositionY(455);
-//        assertEquals(true,cactus1.intersects(bunny));
-//    }
+    public GameModelController gmc;
+    public Bunny bunny;
+    public ArrayList<Floor> floorList;
+    private Floor floor1;
+    private Floor floor2;
+    private Cactus cactus1;
+    private Cactus cactus2;
+    private ArrayList<Cactus> cactusList;
+    private JFXPanel jfxPanel = new JFXPanel();
+
+    @BeforeEach
+    void runBefore(){
+        bunny = new Bunny(50,750);
+        floor1 = new Floor(1000, 800,bunny,0);
+        floor2 = new Floor(1000, 800,bunny,0);
+        cactus1 = new Cactus(floor1);
+        cactus2 = new Cactus(floor2);
+        cactusList = new ArrayList<>();
+        cactusList.add(cactus1);
+        cactusList.add(cactus2);
+    }
+
+    @Test
+    void getPositionYTest(){
+        assertEquals(floor1.getPositionY()-50,cactus1.getPositionY());
+        assertEquals(floor2.getPositionY()-50,cactus2.getPositionY());
+    }
+
+    @Test
+    void getCactusImage(){
+        assertEquals(cactus1.getImage().getWidth(),cactus2.getImage().getWidth());
+        assertEquals(cactus2.getImage().getHeight(),cactus2.getImage().getHeight());
+    }
+
+    @Test
+    void checkIsTouchedTest() {
+        cactus2.setPositionY(750);
+        cactus2.setPositionX(50);
+        cactus1.setPositionY(750);
+        cactus1.setPositionX(75);
+
+        bunny.checkTouchCactus(cactusList);
+        assertEquals(1,bunny.getHp());
+        assertEquals(true,cactus1.getIsTounched());
+        assertEquals(true,cactus1.getIsTounched());
+    }
+
+    @Test
+    void setIsTouchTest() {
+        cactus1.setIsTouched(true);
+        assertEquals(true,cactus1.getIsTounched());
+    }
+
+
+
+    @Test
+    void updateTest(){
+        cactus1.setPositionX(500);
+        cactus1.update(1);
+        assertEquals(400,cactus1.getPositionX());
+        assertEquals(101,cactus1.getVelocityX());
+
+        cactus1.update(4);
+        assertEquals(-4,cactus1.getPositionX());
+        assertEquals(102,cactus1.getVelocityX());
+    }
+
+    @Test
+    void getBoundaryTest() {
+        assertEquals(new Rectangle2D(cactus1.getPositionX(),cactus1.getPositionY(),
+                cactus1.getWidth() - 5,cactus1.getHeight() - 5), cactus1.getBoundary());
+    }
+
+    @Test
+    void intersectsTest() {
+        cactus1.setPositionX(500);
+        cactus1.setPositionY(500);
+        bunny.setPositionX(500);
+        bunny.setPositionY(455);
+        assertEquals(true,cactus1.intersects(bunny));
+    }
 
 
 }
--- src/test/system/PlayerWriterTest.java
+++ src/test/system/PlayerWriterTest.java
@@ -14,39 +14,39 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class PlayerWriterTest {
-//    private static final String TEST_FILE = "./data/testPlayers2";
-//    private PlayerWriter testPlayerWriter;
-//    private Player player1;
-//    private Player player2;
-//
-//    @BeforeEach
-//    void runBefore() throws FileNotFoundException, UnsupportedEncodingException {
-//        testPlayerWriter = new PlayerWriter(new File(TEST_FILE));
-//        player1 = new Player("test1", "pass1",0.0);
-//        player2 = new Player("test2", "pass2",1.0);
-//    }
-//
-//    @Test
-//    void testWriteAccounts() {
-//        // save chequing and savings accounts to file
-//        testPlayerWriter.write(player1);
-//        testPlayerWriter.write(player2);
-//        testPlayerWriter.close();
-//
-//        // now read them back in and verify that the accounts have the expected values
-//        try {
-//            List<Player> players = PlayerReader.readAccounts(new File("./data/testPlayers2"));
-//            Player player1 = players.get(0);
-//            assertEquals("test1", player1.getUsername());
-//            assertEquals("pass1", player1.getPassword());
-//            assertEquals(0.0, player1.getRecord());
-//
-//            Player player2 = players.get(1);
-//            assertEquals("test2", player2.getUsername());
-//            assertEquals("pass2", player2.getPassword());
-//            assertEquals(1.0, player2.getRecord());
-//        } catch (IOException e) {
-//            fail("IOException should not have been thrown");
-//        }
-//    }
+    private static final String TEST_FILE = "./data/testPlayers2";
+    private PlayerWriter testPlayerWriter;
+    private Player player1;
+    private Player player2;
+
+    @BeforeEach
+    void runBefore() throws FileNotFoundException, UnsupportedEncodingException {
+        testPlayerWriter = new PlayerWriter(new File(TEST_FILE));
+        player1 = new Player("test1", "pass1",0.0);
+        player2 = new Player("test2", "pass2",1.0);
+    }
+
+    @Test
+    void testWriteAccounts() {
+        // save chequing and savings accounts to file
+        testPlayerWriter.write(player1);
+        testPlayerWriter.write(player2);
+        testPlayerWriter.close();
+
+        // now read them back in and verify that the accounts have the expected values
+        try {
+            List<Player> players = PlayerReader.readAccounts(new File("./data/testPlayers2"));
+            Player player1 = players.get(0);
+            assertEquals("test1", player1.getUsername());
+            assertEquals("pass1", player1.getPassword());
+            assertEquals(0.0, player1.getRecord());
+
+            Player player2 = players.get(1);
+            assertEquals("test2", player2.getUsername());
+            assertEquals("pass2", player2.getPassword());
+            assertEquals(1.0, player2.getRecord());
+        } catch (IOException e) {
+            fail("IOException should not have been thrown");
+        }
+    }
 }
